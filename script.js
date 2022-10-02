ff49f989c26de28e4d958653adf365a14232f308
MrMegaWatt13 <112874353+MrMegaWatt13@users.noreply.github.com>
2022-10-02 10:26:46 +0800

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var ping = 100;
 
 window.setInterval(function(){
-
        ping = ping + 1
-
+       document.getElementById("ping").innerHTML = ping;
 }, 10)
 
 window.setInterval(function(){
-
        ping = ping - 1;
+       document.getElementById("ping").innerHTML = ping;
+}, 11)
 
-}, 10)
+if(ping >= 200){
+   ping = 100
+}
